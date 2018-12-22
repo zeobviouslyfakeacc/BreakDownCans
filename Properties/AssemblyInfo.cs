75cc5b7741bb0534e91e08e826fc84cd7151df38
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-12-22 15:45:47 +0100

    Set assembly version to 1.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1f95c8f5-b9e2-4f98-8e5b-819dd8bca216")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
